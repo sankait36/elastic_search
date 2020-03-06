9d4d2e5cfac91bf07cc684020ac355b1dfd873ba
sankait36 <sankait36@hotmail.com>
2020-03-06 12:29:13 -0600

    Adds searchbar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Result from './Result';
+import Searchbox from './Searchbox';
 
 import { baseUrl } from '../utils/api';
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('death');
+  const [searchTerm, setSearchTerm] = useState('taxes');
   const [searchOffset, setSearchOffset] = useState(0);
+
+  const [numHits, setNumHits] = useState(0);
 
   useEffect(() => {
     const search = async () => {
       axios.get(`${baseUrl}/search`, { params: { term: searchTerm, offset: searchOffset } })
         .then(res => {
+          setNumHits(res.data.hits.total);
           setSearchResults(res.data.hits.hits);
         })
         .catch(err => err)
@@ -23,6 +27,9 @@ const App = () => {
 
   return (
     <div className="container">
+      <div className="header">
+        <Searchbox />
+      </div>
       <Result
         searchResults={searchResults}
       />
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import '../sass/main.scss';
+import './sass/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
